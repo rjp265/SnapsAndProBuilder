1e02e65dcdc6dec23049c0cf24160a4ab42329db
Robert Perez <49209439+rjp265@users.noreply.github.com>
2019-12-10 20:31:58 -0500

    Initial commit

--- Assets/Standard Assets/Utility/FPSCounter.cs
+++ Assets/Standard Assets/Utility/FPSCounter.cs
@@ -1,38 +1,22 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
-namespace UnityStandardAssets.Utility
+public class CountDown : MonoBehaviour
 {
-    [RequireComponent(typeof (Text))]
-    public class FPSCounter : MonoBehaviour
+    float timeLeft = 300.0f;
+
+    public Text text;
+
+
+
+    void Update()
     {
-        const float fpsMeasurePeriod = 0.5f;
-        private int m_FpsAccumulator = 0;
-        private float m_FpsNextPeriod = 0;
-        private int m_CurrentFps;
-        const string display = "{0} FPS";
-        private Text m_Text;
-
-
-        private void Start()
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Left:" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
         {
-            m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
-            m_Text = GetComponent<Text>();
-        }
-
-
-        private void Update()
-        {
-            // measure average frames per second
-            m_FpsAccumulator++;
-            if (Time.realtimeSinceStartup > m_FpsNextPeriod)
-            {
-                m_CurrentFps = (int) (m_FpsAccumulator/fpsMeasurePeriod);
-                m_FpsAccumulator = 0;
-                m_FpsNextPeriod += fpsMeasurePeriod;
-                m_Text.text = string.Format(display, m_CurrentFps);
-            }
+            Application.LoadLevel("gameOver");
         }
     }
 }
